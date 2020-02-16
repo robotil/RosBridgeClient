db9eec785ef88d2c78992181a85277f41b49205a
michele <mhallakstamler@gmail.com>
2020-02-16 14:59:44 +0200

    Build library for Smart Loader project

---
+++ Messages/Mavros/OverrideRCIn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace RosSharp.RosBridgeClient.Messages.Mavros
+{
+    class OverrideRCIn : Message
+    {
+        /**
+        # Override RC Input
+        # Currently MAVLink defines override for 8 channels
+    **/
+
+        [JsonIgnore]
+        public const string RosMessageName = "mavros_msgs/OverrideRCIn";
+        public UInt16 CHAN_RELEASE = 0;
+        public UInt16 CHAN_NOCHANGE = 65535;
+        
+        public UInt16[] channels;
+
+        public OverrideRCIn()
+        {
+            CHAN_RELEASE = 0;
+            CHAN_NOCHANGE = 65535;
+            channels = new UInt16[8];
+            
+        }
+    }
+}
+
