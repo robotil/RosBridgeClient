e9009913ba9b9ed41f4f3b558f362162868567d5
olugashi <orenlugashi@gmail.com>
2021-01-27 11:03:32 +0200

    Update git ignore.
    Insert MultiArray messages

---
+++ Messages/Standard/Float64MultiArray.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using RosSharp.RosBridgeClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RosBridgeClient.Messages.Standard
+{
+    public class Float64MultiArray : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "std_msgs/Float64MultiArray";
+
+        public MultiArrayLayout Layout;
+        public double[] data;
+
+        public Float64MultiArray()
+        {
+            Layout = new MultiArrayLayout();
+            data = new double[0];
+        }
+    }
+
+}
---
+++ Messages/Standard/MultiArrayDimension.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using RosSharp.RosBridgeClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RosBridgeClient.Messages.Standard
+{
+    public class MultiArrayDimension : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "std_msgs/MultiArrayDimension";
+
+        public string Label;
+        public uint Size;
+        public uint Stride;
+
+        public MultiArrayDimension()
+        {
+            Label = string.Empty;
+            Size = 0;
+            Stride = 0;
+        }
+    }
+}
